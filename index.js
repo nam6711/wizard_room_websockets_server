8ede01203d91d0639d3afafb57cfdf5ef7b107d8
noshpineapple28 <manoucherin@gmail.com>
2024-03-30 23:32:02 -0400

    fix: heroku error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, Server } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+// const wss = new WebSocketServer({ port: 8080 });
+const wss = new Server({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
