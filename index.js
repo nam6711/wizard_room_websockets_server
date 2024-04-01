37c4d8b729718fae6260ae617f8cd51107a18457
noshpineapple28 <manoucherin@gmail.com>
2024-03-31 22:00:15 -0400

    feat: UDP server connection

--- index.js
+++ index.js
@@ -8,15 +8,37 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+const UDP_CLIENT = {
+  client: null,
+  connected: false,
+}
 
 const wss = new Server({ server });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  console.log("CONNECTED");
+
+  ws.on('message', (data) => {
+    data = data.toString();
+    if (data === "UDP") {
+      console.log('UDP CLIENT CONNECTED');
+      UDP_CLIENT.client = ws;
+      UDP_CLIENT.connected = true;
+    } else if (UDP_CLIENT.connected) {
+      console.log("COMMAND SENT: %s", data);
+      UDP_CLIENT.client.send("servo");
+    }
   });
 
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    if (ws === UDP_CLIENT.client) {
+      console.log('UDP CLIENT DISCONNECTED');
+      UDP_CLIENT.address = null;
+      UDP_CLIENT.connected = false;
+    } else {
+      console.log("client closed");
+    }
+  });
 
-  ws.send('something');
+  ws.send('welcome');
 });
